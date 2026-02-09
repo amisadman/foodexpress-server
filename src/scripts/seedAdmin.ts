0d8b2d5eaa152bdeb2b56ef7cfbdcd6302611c5a
amisadman <inboxsadman@gmail.com>
2026-02-09 12:29:54 +0600

    feat: added seed admin

---
+++ src/scripts/seedAdmin.ts
@@ -0,0 +1,32 @@
+import { Role } from "../../generated/prisma/enums";
+import { env } from "../config/env";
+import { auth } from "../lib/auth";
+import { prisma } from "../lib/prisma";
+
+export const seedAdmin = async () => {
+  console.log("Running seed Admin script...");
+
+  const existingAdmin = await prisma.user.findUnique({
+    where: {
+      email: env.adminEmail as string,
+    },
+  });
+
+  if (existingAdmin) {
+    console.log("Admin already exists!");
+    return;
+  }
+
+  const data = await auth.api.signUpEmail({
+    body: {
+      name: "Admin",
+      email: env.adminEmail as string,
+      password: env.adminPass as string,
+      role: Role.ADMIN,
+    },
+  });
+
+  console.log("Admin seeded successfully");
+
+  console.log(data);
+};
--- src/server.ts
+++ src/server.ts
@@ -1,6 +1,7 @@
 import app from "./app";
 import { env } from "./config/env";
 import { db } from "./database/db";
+import { seedAdmin } from "./scripts/seedAdmin";
 
 const server = async () => {
   try {
@@ -8,6 +9,7 @@ const server = async () => {
     app.listen(env.port, () => {
       console.log(`Server is Running at: http://localhost:${env.port}`);
     });
+    seedAdmin();
   } catch (error) {
     console.log(error);
     await db.disConnectDB();
