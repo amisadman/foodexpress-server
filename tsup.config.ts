1a614ae04ab6b8888e6841219c2a4f97147ff52e
amisadman <inboxsadman@gmail.com>
2026-06-15 06:33:41 +0600

    feat: added queryBuilder.ts

--- tsup.config.ts
+++ tsup.config.ts
@@ -1,7 +1,7 @@
 import { defineConfig } from "tsup";
 
 export default defineConfig({
-  entry: ["src/server.ts"],
+  entry: ["src/index.ts"],
   format: ["esm"],
   platform: "node",
   target: "node20",
